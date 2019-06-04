2d6f9437e40f150ee9b4c9037f5823d5e3999a5e
paradisa <paradisa@student.ethz.ch>
2019-06-04 18:48:39 +0200

    added keepAlive, is exactly ARC so no new proofs here

---
+++ examples/serialExecutor/serialExecutorWKeepAlive.cpp
@@ -0,0 +1,63 @@
+//from folly/executors/SerialExecutor.cpp
+//No keepAliveCounter, we'll see about it later
+
+class SerialExecutor{
+	Executor parent_;
+	size_t scheduled_; //atomic
+	UnboundedQueue queue_; //multiple producers single consumer
+	int keepAliveCounter_;
+
+	SerialExecutor(parent){
+		parent_ = parent;
+		queue = newQueue();
+		scheduled_ = 0;
+		keepAliveCounter_ = 1;
+	}
+
+	void add(Func func){
+		queue_.enqueue(func);
+		parent_->add({this->run()});
+	}
+
+	void run(){
+		if(scheduled_.fetch_add(1, acquire) > 0)
+			return;
+		do {
+			Func func = queue_.dequeue();
+			func();
+		} while(scheduled_.fetch_sub(1, release) > 1);
+	}
+
+	void keepAliveAcquire(){
+		int c = keepAliveCounter_.fetch_add(1, acquire);
+	}
+
+	void keepAliveRelease(){
+		int c = keepAliveCounter_.fetch_sub(1, release);
+		if(c == 1)
+			delete this;
+	}
+
+	KeepAlive create(Executor parent){
+		return KeepAlive(SerialExecutor(parent));
+	}
+}
+
+class KeepAlive{
+	Executor executor_;
+
+	KeepAlive(Executor executor) : executor_(executor)
+
+	~KeepAlive() {
+		executor.keepAliveRelease();
+	}
+
+	Executor get(){
+		return executor;
+	}
+
+	KeepAlive copy(){
+		executor.keepAliveAcquire();
+		return KeepAlive(executor);
+	}
+}
